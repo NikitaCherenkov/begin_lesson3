ae8386544b7ece0a48bf1853b4053ed3d1763870
Nikita <cherenkov204@gmail.com>
2022-03-13 14:52:28 +0300

    task19 & task23 done

---
+++ task19/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите число");
+String symb = Convert.ToString(Console.ReadLine());
+int i = 0;
+Boolean stop = false;
+while (i < symb.Length / 2 & !stop) {
+    if (symb[i] != symb[symb.Length - i - 1]) {
+        stop = true;
+    }
+    i++;
+}
+if (symb.Length != 5) {
+    Console.WriteLine("Введено не пятизначное число");
+}
+if (stop) {
+    Console.WriteLine("Число не является палиндромом");
+} else {
+    Console.WriteLine("Введён палиндром");
+}
---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{N} ->");
+int i = 0;
+while (i < N - 1) {
+    i = i + 1;
+    Console.Write($" {i * i * i},");
+}
+if (N >= 1) {
+    Console.Write($" {(i + 1) * (i + 1) * (i + 1)}");
+} else {
+    Console.Write(" null");
+}
